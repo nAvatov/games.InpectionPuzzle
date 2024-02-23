7219e7816ad2401daa78a8625575592c88a86f52
Nariman Avatov <110205226+nAvatov@users.noreply.github.com>
2024-02-23 14:45:20 +0300

    Additional rotation gestures

--- Assets/Scripts/LockedCamera.cs
+++ Assets/Scripts/LockedCamera.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 using Zenject;
 
 public class LockedCamera : ILockedCamera {
-    private const float _cameraMoveSpeed = 0.5f;
+    private const float _cameraMoveSpeedButtons = 0.5f;
+    private const float _cameraMoveSpeedGestures = 1f;
     private Transform _targetTransform;
     private Transform _cameraTransform;
     private Vector3 _zoomInRestriction;
+    private Vector3 _rotationVectorByGesture;
+    private Vector2 _gesturePosDelta;
     
     [Inject]
     public void Construct(Camera cam, ITargetObject targetObject) {
@@ -17,16 +20,23 @@ public class LockedCamera : ILockedCamera {
 
     public void ChangeDistanceToObject(Vector3 direction) {
         if (direction == Vector3.forward && Vector3.Distance(_cameraTransform.position, _zoomInRestriction) > 0) {
-            _cameraTransform.Translate(direction * _cameraMoveSpeed * Time.deltaTime, Space.World);
-            _cameraTransform.LookAt(_targetTransform);
+            _cameraTransform.Translate(direction * _cameraMoveSpeedButtons * Time.deltaTime, Space.World);
         } else {
-            _cameraTransform.Translate(direction * _cameraMoveSpeed * Time.deltaTime, Space.World);
-            _cameraTransform.LookAt(_targetTransform);
+            _cameraTransform.Translate(direction * _cameraMoveSpeedButtons * Time.deltaTime, Space.World);
         }
     }
 
     public void MoveAroundObject(Vector3 direction) {
-        //_cameraTransform.Translate(direction * _cameraMoveSpeed * Time.deltaTime, Space.World);
-        _cameraTransform.RotateAround(_targetTransform.position, direction, _cameraMoveSpeed);
+        _cameraTransform.RotateAround(_targetTransform.position, direction, _cameraMoveSpeedButtons);
+    }
+
+    public void RotateCameraByGesture(Vector2 gestureStartPos, Vector2 gestureCurrentPos) {
+        _gesturePosDelta = gestureCurrentPos - gestureStartPos;
+
+        _rotationVectorByGesture.x = -_gesturePosDelta.y;
+        _rotationVectorByGesture.y = _gesturePosDelta.x;
+        _rotationVectorByGesture.z = 0;
+
+        _cameraTransform.RotateAround(_targetTransform.position, _rotationVectorByGesture, _cameraMoveSpeedGestures);
     }
 }
--- Assets/Scripts/LockedCameraController.cs
+++ Assets/Scripts/LockedCameraController.cs
@@ -9,6 +9,8 @@ public class LockedCameraController: IInitializable {
     private ILockedCamera _lockedCamera;
     private CameraControlButtons _cameraControlButtons;
     private CompositeDisposable _disposables;
+    private Vector2 _mousePressPoint;
+    private Vector2 _mousePressPointDelta;
 
     [Inject] 
     public void Construct(ILockedCamera cam, CameraControlButtons cameraControlButtons, CompositeDisposable disposables) {
@@ -23,6 +25,32 @@ public class LockedCameraController: IInitializable {
         AssingActionToButton(_cameraControlButtons.ZoomOut, () => _lockedCamera.ChangeDistanceToObject(Vector3.back));
         AssingActionToButton(_cameraControlButtons.MoveAroundLeft, () => _lockedCamera.MoveAroundObject(Vector3.down));
         AssingActionToButton(_cameraControlButtons.MoveAroundRight, () => _lockedCamera.MoveAroundObject(Vector3.up));
+
+        AssingMouseGestures();
+    }
+
+    private void AssingMouseGestures() {
+        Observable
+            .EveryUpdate()
+            .Where(_ => {
+                return Input.GetMouseButtonDown(0);
+            })
+            .Subscribe(_ => {
+                _mousePressPoint = Input.mousePosition;
+            })
+            .AddTo(_disposables);
+
+        Observable
+            .EveryUpdate()
+            .Where(_ => {
+                return Input.GetMouseButton(0);
+            })
+            .Subscribe(_ => {
+                _lockedCamera.RotateCameraByGesture(_mousePressPoint, new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+                _mousePressPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y); 
+            })
+            .AddTo(_disposables);
     }
 
     private void AssingActionToButton(UIButtonAdditional button, System.Action action) {
