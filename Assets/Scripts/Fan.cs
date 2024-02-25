0f6ddefa52cb2b8f4a9046ad51b2e36ed8d60170
Nariman Avatov <110205226+nAvatov@users.noreply.github.com>
2024-02-25 14:47:40 +0300

    Switching blades rotation

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
@@ -8,6 +6,7 @@ using DG.Tweening;
 
 public class Fan : IFanInteractive, ITargetObject {
     private CompositeDisposable _disposables;
+    private bool _isOn = false;
     private FanPartsStruct _fanParts;
     public Transform targetTransform => _fanParts.ViewCenter;
     public FanPartsStruct FanParts => _fanParts;
@@ -24,8 +23,43 @@ public class Fan : IFanInteractive, ITargetObject {
     private void SignSwitchStream() {
         _fanParts.SwitchButton.OnMouseDownAsObservable()
             .Subscribe(_ => {
-                Debug.Log("Click");
+                if (!_isOn) {
+                    SwitchOnAnimation();
+                } else {
+                    SwitchOffAnimation();
+                }
+
+                _isOn = !_isOn;
             })
             .AddTo(_disposables);
+    }
+
+    private void SwitchOnAnimation() {
+        _fanParts.SwitchButton.enabled = false;
+
+        _fanParts.FanBlades
+            .DORotate(new Vector3(0, 0, 360f), 1f)
+            .SetRelative(true)
+            .SetEase(Ease.InSine)
+            .OnComplete(() => {
+                _fanParts.SwitchButton.enabled = true;
+                _fanParts.FanBlades.DORotate(new Vector3(0, 0, 360f), 0.5f, RotateMode.FastBeyond360)
+                    .SetRelative(true)
+                    .SetEase(Ease.Linear)
+                    .SetLoops(-1);
+            });
+    }
+
+    private void SwitchOffAnimation() {
+        _fanParts.SwitchButton.enabled = false;
+        
+        _fanParts.FanBlades
+            .DORotate(new Vector3(0, 0, 360f), 1f)
+            .SetRelative(true)
+            .SetEase(Ease.OutSine)
+            .OnComplete(() => {
+                _fanParts.FanBlades.DOKill();
+                _fanParts.SwitchButton.enabled = true;
+            });
     }
 }
