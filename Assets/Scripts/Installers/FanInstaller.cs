4b8b1d1a66034344c8d69629a4a1d0ebec66969d
Nariman Avatov <110205226+nAvatov@users.noreply.github.com>
2024-02-28 16:16:17 +0300

    main installer now holds only common services each fan has attached camera room model imported IntaractableFan prefab holds attached camera, model and attached canvas with control button projecting on particular camera

--- Assets/Scripts/Installers/FanInstaller.cs
+++ Assets/Scripts/Installers/FanInstaller.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 using Zenject;
 
 public class FanInstaller : MonoInstaller {
+    [Header("Attached Camera Dependencies")]
+    [SerializeField] private Camera _attacheCamera;
+    [SerializeField] private CameraControlButtonsStruct _cameraControlButtons;
+    [SerializeField] private LockedCameraConfiguration _cameraConfiguration;
+
+    [Header("Fan Dependencies")]
     [SerializeField] private FanPartsStruct _fanParts;
     [SerializeField] private HingeConfigurationStruct _hingeConfiguration;
     [SerializeField] private BladesConfigurationStruct _bladesConfiguration;
     [SerializeField] private BodyConfigurationStruct _bodyConfiguration;
-    [SerializeField] private AudioSource _fanAudioSource;
     [SerializeField] private SoundConfigurationStruct _soundConfiguration;
     public override void InstallBindings() {
-        Container.BindInstance(_fanAudioSource);
+        BindFanDependencies();
+        BindAttachedCameraDependencies();
+    }
+
+    private void BindFanDependencies() {
         Container.BindInstance(_soundConfiguration);
         Container.BindInstance(_fanParts);
 
@@ -22,5 +31,13 @@ public class FanInstaller : MonoInstaller {
         Container.Bind<Hinge>().AsSingle();
 
         Container.BindInterfacesAndSelfTo<FanController>().AsSingle().NonLazy();
+    }
+
+    private void BindAttachedCameraDependencies() {
+        Container.BindInstance(_cameraConfiguration);
+        Container.BindInstance(_cameraControlButtons);
+        Container.BindInstance(_attacheCamera);
+        Container.Bind<ILockedCamera>().To<LockedCamera>().AsSingle();
+        Container.Bind(typeof(IInitializable)).To<LockedCameraController>().AsSingle().NonLazy();
     }
 }
--- Assets/Scripts/Installers/MainInstaller.cs
+++ Assets/Scripts/Installers/MainInstaller.cs
@@ -4,38 +4,23 @@ using Zenject;
 
 public class MainInstaller : MonoInstaller {
     private CompositeDisposable _disposables = new CompositeDisposable();
-    [Header("Camera")]
-    [SerializeField] private Camera _mainCamera;
-    [SerializeField] private CameraControlButtonsStruct _cameraControlButtons;
-    [SerializeField] private LockedCameraConfiguration _cameraConfiguration;
+    [Header("Audio")]
+    [SerializeField] private AudioSource _audioSource;
+
     [Header("Hover notifications")]
     [SerializeField] private UI_Notifier _notifier;
     [SerializeField] private HoverConfigurationStruct _hoverConfiguration;
-    [Header("Sub")]
-    [SerializeField] private GameObjectContext _fanContext;
     
     public override void InstallBindings() {
-        _fanContext.Install(Container);
-        
         Container.Bind<CompositeDisposable>().FromInstance(_disposables);
 
-        Container.BindInstance(_fanContext.Container.Resolve<ITargetObject>());
-        Container.BindInstance(_hoverConfiguration);
-
-        BindCameraService();
         BindNotificationService();
-    }
-
-    private void BindCameraService() {
-        Container.BindInstance(_cameraConfiguration);
-        Container.BindInstance(_cameraControlButtons);
-        Container.BindInstance(_mainCamera);
-        Container.Bind<ILockedCamera>().To<LockedCamera>().AsSingle();
-        Container.Bind(typeof(IInitializable)).To<LockedCameraController>().AsSingle().NonLazy();
+        Container.BindInstance(_audioSource);
     }
 
     private void BindNotificationService() {
         Container.BindInstance(_notifier).AsSingle();
+        Container.BindInstance(_hoverConfiguration);
     }
 
     private void OnDestroy() {
